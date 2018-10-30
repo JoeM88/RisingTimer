faa895ae2e7f50c805169bb76fc9f8a613821601
Joseph Molina <josephmolina3388@gmail.com>
2018-10-30 12:27:56 -0700

    Implemented Settings Screen

---
+++ app/src/main/java/e/josephmolina/risingtimer/SettingsActivityFragment.kt
@@ -0,0 +1,10 @@
+package e.josephmolina.risingtimer
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsActivityFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
+    }
+}
--- app/src/main/java/e/josephmolina/risingtimer/Util/PrefUtil.kt
+++ app/src/main/java/e/josephmolina/risingtimer/Util/PrefUtil.kt
@@ -1,15 +1,16 @@
 package e.josephmolina.risingtimer.Util
 
 import android.content.Context
-import android.preference.Preference
 import android.preference.PreferenceManager
 import e.josephmolina.risingtimer.TimerActivity
 
 class PrefUtil {
     companion object {
+        private const val TIMER_LENGTH_ID = "e.josephmolina.risingtimer.timer.timer_length"
+
         fun getTimerLength(context: Context): Int {
-            // placeholder
-            return 1
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getInt(TIMER_LENGTH_ID, 10)
         }
 
         private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID = "e.josephmolina.risingtimer.previous_timer_length"
